5145c4d1b6e9f0f9d543629323e10da904327f03
Hasan Sahin <sahinhhasann@gmail.com>
2023-02-11 22:38:50 +0300

    create grpc server

---
+++ grpc/app.js
@@ -0,0 +1,35 @@
+const PROTO_PATH = './user.proto'
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
+require('../database/connection')
+const User = require('../models/user')
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	arrays: true,
+})
+
+const userProto = grpc.loadPackageDefinition(packageDefinition)
+const server = new grpc.Server()
+server.addService(userProto.UserService.service, {
+	createNewUser: async (call, callback) => {
+		const reqBody = call.request.user
+		const user = new User(reqBody)
+		const result = await user.save()
+		if (!result) {
+			console.log('HATA')
+		}
+		callback(null, { user })
+	},
+})
+
+server.bindAsync(
+	'127.0.0.1:3000',
+	grpc.ServerCredentials.createInsecure(),
+	(error, port) => {
+		server.start()
+		console.log('Server 3000 portundan ayaklandırıldı')
+	}
+)
